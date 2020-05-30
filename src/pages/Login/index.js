f5e689c8d39873d1c528681ac5c14cc5233e94f9
Arun Sood <arsood@gmail.com>
2020-05-30 01:01:26 -0700

    Login page UI

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import LogoSplitLayout from '../../layouts/LogoSplit';
 
@@ -7,7 +8,29 @@ import './Login.scss';
 const Login = () => {
   return (
     <LogoSplitLayout>
-      Other Stuff
+      <div className="p-2">
+        <div className="text-right">
+          <Link to="/sign-up">
+            Sign up
+          </Link>
+        </div>
+
+        <div className="container-small absolute-center-y">
+          <input type="email" className="form-control mb-4" placeholder="Email" />
+
+          <input type="password" className="form-control mb-4" placeholder="Password" />
+
+          <div className="text-center mb-1">
+            <button className="btn btn-primary px-5">Sign in</button>
+          </div>
+
+          <div className="text-center">
+            <Link to="/forgot-password" className="small text-muted">
+              Forgot password
+            </Link>
+          </div>
+        </div>
+      </div>
     </LogoSplitLayout>
   );
 }
