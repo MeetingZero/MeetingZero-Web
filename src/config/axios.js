7a28a6cb19be94f77dd0cac5514f174de1de39b4
Arun Sood <arsood@gmail.com>
2020-06-10 01:14:16 -0700

    Axios requests base url changes

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-let baseUrl;
-
-if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:3000';
-} else if (process.env.REACT_APP_ENV === 'staging') {
-  baseUrl = 'https://api-dev.meetingzero.net';
-} else if (process.env.NODE_ENV === 'production') {
-  baseUrl = 'https://api.meetingzero.net';
-}
-
 export default function() {
+  let baseUrl;
+
+  if (process.env.NODE_ENV === 'development') {
+    baseUrl = 'http://localhost:3000';
+  } else if (process.env.REACT_APP_ENV === 'staging') {
+    baseUrl = 'https://api-dev.meetingzero.net';
+  } else if (process.env.NODE_ENV === 'production') {
+    baseUrl = 'https://api.meetingzero.net';
+  }
+
   let config = {
     baseURL: baseUrl,
     timeout: 10000
