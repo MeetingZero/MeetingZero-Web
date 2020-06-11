b7434d5e3e3b340affb6cda85d81c5cbcfe61136
Arun Sood <arsood@gmail.com>
2020-06-11 11:45:53 -0700

    Revert "Lazy loading for routes instead of components"
    This reverts commit 72afa1266d447bb1787f26e565dde2f1e96b2712.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import store from './app/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 // Bootstrap library
@@ -11,19 +11,59 @@ import './assets/vendor/bootstrap/bootstrap.scss';
 import * as Misc from './constants/misc';
 import LoadingScreen from './library/LoadingScreen';
 
-// Application routes
+// Application pages
 
-const LoginSignup = lazy(() => {
+const Login = lazy(() => {
   return Promise.all([
-    import('./routes/LoginSignup'),
+    import('./pages/Login'),
     new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
   ])
   .then(([moduleExports]) => moduleExports);
 });
 
-const CreateJoinWorkshop = lazy(() => {
+const SignUp = lazy(() => {
   return Promise.all([
-    import('./routes/CreateJoinWorkshop'),
+    import('./pages/SignUp'),
+    new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
+
+const SignUpConfirmation = lazy(() => {
+  return Promise.all([
+    import('./pages/SignUpConfirmation'),
+    new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
+
+const ForgotPassword = lazy(() => {
+  return Promise.all([
+    import('./pages/ForgotPassword'),
+    new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
+
+const ResetPassword = lazy(() => {
+  return Promise.all([
+    import('./pages/ResetPassword'),
+    new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
+
+const JoinWorkshop = lazy(() => {
+  return Promise.all([
+    import('./pages/JoinWorkshop'),
+    new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
+  ])
+  .then(([moduleExports]) => moduleExports);
+});
+
+const CreateWorkshop = lazy(() => {
+  return Promise.all([
+    import('./pages/CreateWorkshop'),
     new Promise(resolve => window.setTimeout(resolve, Misc.LAZY_LOADING_MIN_TIMEOUT))
   ])
   .then(([moduleExports]) => moduleExports);
@@ -35,9 +75,33 @@ ReactDOM.render(
       <Router>
         <Suspense fallback={<LoadingScreen />}>
           <Switch>
-            <LoginSignup />
+            <Route path='/login'>
+              <Login />
+            </Route>
 
-            <CreateJoinWorkshop />
+            <Route path='/sign-up'>
+              <SignUp />
+            </Route>
+
+            <Route path='/signup-confirmation'>
+              <SignUpConfirmation />
+            </Route>
+
+            <Route path='/forgot-password'>
+              <ForgotPassword />
+            </Route>
+
+            <Route path='/reset-password'>
+              <ResetPassword />
+            </Route>
+
+            <Route path='/join-workshop'>
+              <JoinWorkshop />
+            </Route>
+
+            <Route path='/create-workshop'>
+              <CreateWorkshop />
+            </Route>
           </Switch>
         </Suspense>
       </Router>
