465fb2ebd9a35b7d9cc3270d1f187d9f0efbcdf1
Arun Sood <arsood@gmail.com>
2020-07-08 01:16:50 -0700

    Naming

--- src/app/workshop/stages/what_is_working/actions.js
+++ src/app/workshop/stages/what_is_working/actions.js
@@ -24,17 +24,17 @@ export const saveResponse = (workshopToken, responseText) => {
 
 export const getMyResponses = (workshopToken) => {
   return (dispatch) => {
-    dispatch(loadingSlice.actions.startLoading('GET_WHAT_IS_WORKING_RESPONSES'));
+    dispatch(loadingSlice.actions.startLoading('GET_MY_WHAT_IS_WORKING_RESPONSES'));
 
     return axiosInstance()
     .get(`/api/v1/workshops/${workshopToken}/what_is_working`)
     .then((response) => {
-      dispatch(loadingSlice.actions.stopLoading('GET_WHAT_IS_WORKING_RESPONSES'));
+      dispatch(loadingSlice.actions.stopLoading('GET_MY_WHAT_IS_WORKING_RESPONSES'));
 
       return dispatch(whatIsWorkingSlice.actions.setMyWhatIsWorkingResponses(response.data));
     })
     .catch((err) => {
-      dispatch(loadingSlice.actions.stopLoading('GET_WHAT_IS_WORKING_RESPONSES'));
+      dispatch(loadingSlice.actions.stopLoading('GET_MY_WHAT_IS_WORKING_RESPONSES'));
 
       throw err;
     });
