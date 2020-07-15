db169803dd17ab8797d9aa305709fdc529423b95
Arun Sood <arun.sood@coursehero.com>
2020-07-15 13:54:59 -0700

    Function to get all problems responses

--- src/app/workshop/stages/problems/actions.js
+++ src/app/workshop/stages/problems/actions.js
@@ -3,6 +3,25 @@ import axiosInstance from '../../../../config/axios';
 import loadingSlice from '../../../loading/slice';
 import problemsSlice from './slice';
 
+export const getAllResponses = (workshopToken) => {
+  return (dispatch) => {
+    dispatch(loadingSlice.actions.startLoading('GET_ALL_PROBLEMS_RESPONSES'));
+
+    return axiosInstance()
+    .get(`/api/v1/workshops/${workshopToken}/problems`)
+    .then((response) => {
+      dispatch(loadingSlice.actions.stopLoading('GET_ALL_PROBLEMS_RESPONSES'));
+
+      return dispatch(problemsSlice.actions.setAllProblemsResponses(response.data));
+    })
+    .catch((err) => {
+      dispatch(loadingSlice.actions.stopLoading('GET_ALL_PROBLEMS_RESPONSES'));
+
+      throw err;
+    });
+  }
+}
+
 export const getMyResponses = (workshopToken) => {
   return (dispatch) => {
     dispatch(loadingSlice.actions.startLoading('GET_MY_PROBLEMS_RESPONSES'));
