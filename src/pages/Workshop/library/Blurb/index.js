1cb007768db404936473a0810b17175d40c7483d
Arun Sood <arsood@gmail.com>
2020-07-24 00:05:13 -0700

    Show correct icon for discussion allowed

--- src/pages/Workshop/library/Blurb/index.js
+++ src/pages/Workshop/library/Blurb/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import noTalking from 'assets/images/no_talking.svg';
+import talking from 'assets/images/talking.svg';
 import './Blurb.scss';
 
 const Blurb = ({ title, text }) => {
+  const discussionAllowed = useSelector((state) => {
+    return state
+    .Workshop
+    .currentWorkshopStep
+    .workshop_stage_step
+    .discussion_allowed;
+  });
+
   return (
     <React.Fragment>
-      <img src={noTalking} className="blurb-icon ml-auto mb-1" alt="No Talking" />
+      {discussionAllowed ?
+        <img src={talking} className="blurb-icon ml-auto mb-1" alt="Discussion Allowed" />
+      :
+        <img src={noTalking} className="blurb-icon ml-auto mb-1" alt="No Discussion" />
+      }
 
       <div className="border border-primary p-2 rounded shadow">
         <div className="text-muted mb-1">
