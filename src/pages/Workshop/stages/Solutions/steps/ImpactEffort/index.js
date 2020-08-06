b7e45e30a973f40667d7d7b422cf6ab1c698be0e
Arun Sood <arsood@gmail.com>
2020-08-06 00:37:57 -0700

    More styling for impact effort matrix

--- src/pages/Workshop/stages/Solutions/steps/ImpactEffort/index.js
+++ src/pages/Workshop/stages/Solutions/steps/ImpactEffort/index.js
@@ -7,9 +7,25 @@ const ImpactEffort = () => {
     <React.Fragment>
       <h1 className="h2 mt-5">Evaluate Solutions</h1>
 
-      <h5 className="mb-4">Drag and drop each solution (red dot) where you feel it's appropriate based off the impact it will have for your customers and for the business and what the effort needed will be to implement it.</h5>
+      <h5 className="mb-10">Drag and drop each solution (red dot) where you feel it's appropriate based off the impact it will have for your customers and for the business and what the effort needed will be to implement it.</h5>
 
       <div className="impact-effort-chart">
+        <div className="title-top">
+          High Impact
+        </div>
+
+        <div className="title-left">
+          Low Effort
+        </div>
+
+        <div className="title-right">
+          High Effort
+        </div>
+
+        <div className="title-bottom">
+          Low Impact
+        </div>
+
         <div className="impact-effort-chart-row">
           <div className="impact-effort-chart-quadrant border-right border-bottom border-dark">
             <span>Do Now</span>
