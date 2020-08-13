b84a3d48f512688c39eab3246aa158d1bd9489ba
Arun Sood <arsood@gmail.com>
2020-08-13 12:08:49 -0700

    Participant view for hypothesis step

--- src/pages/Workshop/stages/Experiments/steps/Hypothesis/index.js
+++ src/pages/Workshop/stages/Experiments/steps/Hypothesis/index.js
@@ -48,6 +48,10 @@ const Hypothesis = () => {
     return state.Experiments.hypothesis;
   });
 
+  const workshop = useSelector((state) => {
+    return state.Workshop.workshop;
+  });
+
   React.useEffect(() => {
     if (!hypothesis) return;
 
@@ -70,115 +74,133 @@ const Hypothesis = () => {
 
       <h5 className="mb-4">Create a hypothesis that you can test in 1-2 weeks.</h5>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <textarea
-          onChange={(event) => setWeBelieveText(event.target.value)}
-          value={weBelieveText}
-          ref={register({ required: true, maxLength: 140 })}
-          name="we_believe_text"
-          className={cn("form-control mb-1", weBelieveTextCharCountExceeded ? 'bg-scary' : null)}
-          placeholder="We believe <this capability>..."
-        />
-
-        {errors.we_believe_text ?
-          <div className="small text-danger">
-            Please enter a response of 140 characters or less
-          </div>
-        : null}
-
-        <div className="text-right mb-4">
-          <CharacterCounter
-            inputString={weBelieveText}
-            maxChars={140}
-            onExceed={(isExceeded) => setWeBelieveTextCharCountExceeded(isExceeded)}
-          />
-        </div>
-
-        <textarea
-          onChange={(event) => setWillResultInText(event.target.value)}
-          value={willResultInText}
-          ref={register({ required: true, maxLength: 140 })}
-          name="will_result_in_text"
-          className={cn("form-control mb-1", willResultInTextCharCountExceeded ? 'bg-scary' : null)}
-          placeholder="Will result in <this outcome>..."
-        />
-
-        {errors.will_result_in_text ?
-          <div className="small text-danger">
-            Please enter a response of 140 characters or less
-          </div>
-        : null}
-
-        <div className="text-right mb-4">
-          <CharacterCounter
-            inputString={willResultInText}
-            maxChars={140}
-            onExceed={(isExceeded) => setWillResultInTextCharCountExceeded(isExceeded)}
-          />
-        </div>
-
-        <textarea
-          onChange={(event) => setSucceededWhenText(event.target.value)}
-          value={succeededWhenText}
-          ref={register({ required: true, maxLength: 140 })}
-          name="succeeded_when_text"
-          className={cn("form-control mb-1", succeededWhenTextCharCountExceeded ? 'bg-scary' : null)}
-          placeholder="We will know we have succeeded when <we see this measurable signal>..."
-        />
-
-        {errors.succeeded_when_text ?
-          <div className="small text-danger">
-            Please enter a response of 140 characters or less
-          </div>
-        : null}
-
-        <div className="text-right mb-2">
-          <CharacterCounter
-            inputString={succeededWhenText}
-            maxChars={140}
-            onExceed={(isExceeded) => setSucceededWhenTextCharCountExceeded(isExceeded)}
-          />
+      <blockquote className="mb-4">
+        <div className="text-muted small">
+          Winning Solution:
         </div>
 
         <div>
-          <Button
-            type="submit"
-            text={hypothesis === null ? "Submit" : "Update"}
-            className="btn btn-primary px-5 rounded"
-            disabled={weBelieveText.length === 0 || willResultInText.length === 0 || succeededWhenText.length === 0}
-            loading={isLoading}
-          />
+          Winning solution goes here
         </div>
-      </form>
+      </blockquote>
 
-      <ProTip
-        mainText={
-          <React.Fragment>
-            <p>
-              Be mindful of:
-            </p>
+      {workshop.is_host ?
+        <React.Fragment>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <textarea
+              onChange={(event) => setWeBelieveText(event.target.value)}
+              value={weBelieveText}
+              ref={register({ required: true, maxLength: 140 })}
+              name="we_believe_text"
+              className={cn("form-control mb-1", weBelieveTextCharCountExceeded ? 'bg-scary' : null)}
+              placeholder="We believe <this capability>..."
+            />
 
-            <p>
-              <strong>Feasibility risk</strong> - whether the team can build what we need within the time allotted for the experiment.
-            </p>
+            {errors.we_believe_text ?
+              <div className="small text-danger">
+                Please enter a response of 140 characters or less
+              </div>
+            : null}
 
-            <p>
-              If applicable to your MVP, be mindful of:
-            </p>
-
-            <p>
-              <strong>Usability risk</strong> - whether users can figure out how to use it as intended.
-            </p>
-          </React.Fragment>
-        }
-        calloutText={
-          <React.Fragment>
-            <div>
-              <strong>We believe</strong> &lt;putting up 8 mirrors&gt; <strong>will result in</strong> &lt;residents being less annoyed by the wait&gt;. <strong>We will know we have succeeded when</strong> &lt;we get 50% fewer complaints&gt;.
+            <div className="text-right mb-4">
+              <CharacterCounter
+                inputString={weBelieveText}
+                maxChars={140}
+                onExceed={(isExceeded) => setWeBelieveTextCharCountExceeded(isExceeded)}
+              />
             </div>
-          </React.Fragment>
-        }
-      />
+
+            <textarea
+              onChange={(event) => setWillResultInText(event.target.value)}
+              value={willResultInText}
+              ref={register({ required: true, maxLength: 140 })}
+              name="will_result_in_text"
+              className={cn("form-control mb-1", willResultInTextCharCountExceeded ? 'bg-scary' : null)}
+              placeholder="Will result in <this outcome>..."
+            />
+
+            {errors.will_result_in_text ?
+              <div className="small text-danger">
+                Please enter a response of 140 characters or less
+              </div>
+            : null}
+
+            <div className="text-right mb-4">
+              <CharacterCounter
+                inputString={willResultInText}
+                maxChars={140}
+                onExceed={(isExceeded) => setWillResultInTextCharCountExceeded(isExceeded)}
+              />
+            </div>
+
+            <textarea
+              onChange={(event) => setSucceededWhenText(event.target.value)}
+              value={succeededWhenText}
+              ref={register({ required: true, maxLength: 140 })}
+              name="succeeded_when_text"
+              className={cn("form-control mb-1", succeededWhenTextCharCountExceeded ? 'bg-scary' : null)}
+              placeholder="We will know we have succeeded when <we see this measurable signal>..."
+            />
+
+            {errors.succeeded_when_text ?
+              <div className="small text-danger">
+                Please enter a response of 140 characters or less
+              </div>
+            : null}
+
+            <div className="text-right mb-2">
+              <CharacterCounter
+                inputString={succeededWhenText}
+                maxChars={140}
+                onExceed={(isExceeded) => setSucceededWhenTextCharCountExceeded(isExceeded)}
+              />
+            </div>
+
+            <div>
+              <Button
+                type="submit"
+                text={hypothesis === null ? "Submit" : "Update"}
+                className="btn btn-primary px-5 rounded"
+                disabled={weBelieveText.length === 0 || willResultInText.length === 0 || succeededWhenText.length === 0}
+                loading={isLoading}
+              />
+            </div>
+          </form>
+
+          <ProTip
+            mainText={
+              <React.Fragment>
+                <p>
+                  Be mindful of:
+                </p>
+
+                <p>
+                  <strong>Feasibility risk</strong> - whether the team can build what we need within the time allotted for the experiment.
+                </p>
+
+                <p>
+                  If applicable to your MVP, be mindful of:
+                </p>
+
+                <p>
+                  <strong>Usability risk</strong> - whether users can figure out how to use it as intended.
+                </p>
+              </React.Fragment>
+            }
+            calloutText={
+              <React.Fragment>
+                <div>
+                  <strong>We believe</strong> &lt;putting up 8 mirrors&gt; <strong>will result in</strong> &lt;residents being less annoyed by the wait&gt;. <strong>We will know we have succeeded when</strong> &lt;we get 50% fewer complaints&gt;.
+                </div>
+              </React.Fragment>
+            }
+          />
+        </React.Fragment>
+      :
+        <div className="jumbotron text-center bg-info border border-primary">
+          <h4>The group will break silence and discuss out loud while the host writes it out.</h4>
+        </div>
+      }
     </React.Fragment>
   );
 }
