566d226cb83e7b05b7ec7be80f8855dca2595c6f
Arun Sood <arsood@gmail.com>
2020-09-09 11:26:52 -0700

    Host calculates votes before next step for problems

--- src/pages/Workshop/stages/Problems/index.js
+++ src/pages/Workshop/stages/Problems/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import WorkshopApp from 'pages/Workshop/containers/WorkshopApp';
 import Responses from './steps/Responses';
 import Vote from './steps/Vote';
 import ReviewVotes from './steps/ReviewVotes';
 
+import * as workshopActions from 'app/workshop/actions';
+import * as votingActions from 'app/voting/actions';
+
 const Problems = () => {
+  const dispatch = useDispatch();
+
   const currentWorkshopStep = useSelector((state) => {
     return state.Workshop.currentWorkshopStep;
   });
 
   const currentStepKey = currentWorkshopStep.workshop_stage_step.key;
+
+  const workshop = useSelector((state) => {
+    return state.Workshop.workshop;
+  });
+
+  const onTimerExpired = () => {
+    if (workshop.is_host) {
+      const workshopStageStepId = currentWorkshopStep.workshop_stage_step_id;
+
+      dispatch(
+        votingActions
+        .calculateVotingResults(workshop.workshop_token, "ProblemResponse")
+      )
+      .then(() => {
+        dispatch(workshopActions.completeWorkshopStep(workshop.workshop_token, workshopStageStepId));
+      });
+    }
+  }
 
   if (currentStepKey === "PROBLEMS_REPONSES") {
     return (
@@ -21,7 +44,7 @@ const Problems = () => {
     );
   } else if (currentStepKey === "PROBLEMS_VOTE") {
     return (
-      <WorkshopApp>
+      <WorkshopApp onTimerExpired={onTimerExpired}>
         <Vote />
       </WorkshopApp>
     );
