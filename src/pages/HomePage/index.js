7fc736da23392255df19b875064fab612d05812f
Arun Sood <arsood@gmail.com>
2020-09-18 12:33:00 -0700

    Further work on homepage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Button from 'library/Button';
+
 import "./HomePage.scss";
 import logoImg from "assets/images/logo.svg";
 import homeArrowsImg from "assets/images/home_arrows.png";
@@ -15,18 +17,41 @@ const HomePage = () => {
       </div>
 
       <div className="d-flex align-items-end my-3 text-decoration-none mb-5">
-        <img src={logoImg} className="img-fluid d-block" style={{width: 80}} alt="Logo" />
+        <img src={logoImg} className="img-fluid" style={{width: 100}} alt="Logo" />
 
         <div>
-          <h2 className="text-primary font-weight-normal ml-2 d-none d-xl-block">MeetingZero</h2>
+          <h1 className="text-primary font-weight-normal ml-2 d-none d-xl-block">MeetingZero</h1>
 
-          <h3 className="h6 ml-4 d-none d-xl-block text-primary">Your team’s digital facilitator for all your real-time collaborative needs.</h3>
+          <h3 className="h5 ml-4 d-none d-xl-block text-primary font-weight-normal">Your team’s digital facilitator for all your real-time collaborative needs.</h3>
         </div>
       </div>
 
       <div className="row">
         <div className="col-md-6">
-          <img src={homeArrowsImg} className="img-fluid" alt="Arrow Diagram" />
+          <img
+            src={homeArrowsImg}
+            className="img-fluid mb-4"
+            style={{maxWidth: 800}}
+            alt="Arrow Diagram"
+          />
+
+          <div className="mb-1">
+            Get access to our early release!
+          </div>
+
+          <div className="homepage-email-input">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="email address"
+            />
+
+            <Button
+              type="submit"
+              className="btn btn-primary"
+              text="Submit"
+            />
+          </div>
         </div>
 
         <div className="col-md-6">
