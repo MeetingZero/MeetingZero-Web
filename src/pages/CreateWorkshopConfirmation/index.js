dba38ac2e832445e8812bd91375698897e14ef34
Arun Sood <arsood@gmail.com>
2020-09-23 10:03:11 -0700

    Go to dashboard link

--- src/pages/CreateWorkshopConfirmation/index.js
+++ src/pages/CreateWorkshopConfirmation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import LogoSplitLayout from 'layouts/LogoSplit';
 
@@ -17,9 +18,15 @@ const CreateWorkshopConfirmation = () => {
           We've sent an email to {currentUser.email}.
         </p>
 
-        <p>
+        <p className="mb-5">
           Follow the instructions there on <br /> how to get everyone prepared for <br /> the workshop.
         </p>
+
+        <div className="text-center">
+          <Link to="/dashboard" className="h5 font-weight-normal">
+            Go to Dashboard
+          </Link>
+        </div>
       </div>
     </LogoSplitLayout>
   );
