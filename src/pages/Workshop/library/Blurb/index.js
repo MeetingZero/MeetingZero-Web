aeee82c30f8119a5896357d66bf94b9e83c4f35c
Arun Sood <arsood@gmail.com>
2020-09-30 13:24:02 -0700

    Show and hide blurb based on cookies

--- src/pages/Workshop/library/Blurb/index.js
+++ src/pages/Workshop/library/Blurb/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Overlay, Tooltip } from 'react-bootstrap';
+import moment from 'moment';
+
+import cookies from 'config/cookies';
 
 import noTalking from 'assets/images/no_talking.svg';
 import talking from 'assets/images/talking.svg';
@@ -15,10 +18,51 @@ const Blurb = ({ title, text }) => {
     .discussion_allowed;
   });
 
+  const workshop = useSelector((state) => {
+    return state.Workshop.workshop;
+  });
+
   const noTalkingRef = React.useRef(null);
   const discussionAllowedRef = React.useRef(null);
   const [showNoTalkingTooltip, setShowNoTalkingTooltip] = React.useState(false);
   const [showDiscussionAllowedTooltip, setShowDiscussionAllowedTooltip] = React.useState(false);
+  const [showBlurbCallout, setShowBlurbCallout] = React.useState(false);
+
+  React.useEffect(() => {
+    let blurbCalloutViewed;
+
+    if (discussionAllowed) {
+      blurbCalloutViewed = cookies.get("blurbCalloutViewedDiscussionAllowed");
+    } else {
+      blurbCalloutViewed = cookies.get("blurbCalloutViewedDiscussionNotAllowed");
+    }
+
+    if (blurbCalloutViewed && blurbCalloutViewed[workshop.workshop_token]) {
+      setShowBlurbCallout(false);
+    } else {
+      setShowBlurbCallout(true);
+    }
+  }, [discussionAllowed, workshop.workshop_token]);
+
+  const handleHideBlurbCallout = () => {
+    if (discussionAllowed) {
+      cookies.set("blurbCalloutViewedDiscussionAllowed", {
+        [workshop.workshop_token]: true
+      }, {
+        path: "/",
+        expires: moment().add(1, "month").toDate()
+      });
+    } else {
+      cookies.set("blurbCalloutViewedDiscussionNotAllowed", {
+        [workshop.workshop_token]: true
+      }, {
+        path: "/",
+        expires: moment().add(1, "month").toDate()
+      });
+    }
+
+    setShowBlurbCallout(false);
+  }
 
   return (
     <div className="blurb-container">
@@ -62,21 +106,26 @@ const Blurb = ({ title, text }) => {
           <img
             src={noTalking}
             ref={noTalkingRef}
-            onMouseEnter={() => setShowNoTalkingTooltip(true)}
+            onMouseEnter={() => {
+              handleHideBlurbCallout();
+              setShowNoTalkingTooltip(true);
+            }}
             onMouseLeave={() => setShowNoTalkingTooltip(false)}
             className="blurb-icon ml-auto mb-1"
             alt="No Discussion"
           />
+
+          {showBlurbCallout ?
+            <div className="blurb-callout blurb-callout-danger">
+              <button className="blurb-callout-button">
+                Close
+              </button>
+
+              This icon means no talking during this step.
+            </div>
+          : null}
         </React.Fragment>
       }
-
-      <div className="blurb-callout">
-        <button className="blurb-callout-button">
-          Close
-        </button>
-
-        This icon means no talking during this step.
-      </div>
 
       <div className="border border-primary p-2 rounded shadow">
         <div className="text-muted mb-1">
