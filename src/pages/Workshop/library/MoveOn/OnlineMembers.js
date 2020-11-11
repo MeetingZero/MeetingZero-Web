9ce265de3eac38b3b6c7ffab5879e034325ec770
Arun Sood <arsood@gmail.com>
2020-11-11 00:36:29 -0800

    Ready up UI flow

--- src/pages/Workshop/library/MoveOn/OnlineMembers.js
+++ src/pages/Workshop/library/MoveOn/OnlineMembers.js
@@ -4,13 +4,13 @@ import cn from 'classnames';
 
 import "./OnlineMembers.scss";
 
-const OnlineMembers = () => {
+const OnlineMembers = ({ className }) => {
   const workshopMembers = useSelector((state) => {
     return state.Workshop.workshopMembers;
   });
 
   return (
-    <div className="online-members">
+    <div className={cn("online-members", className)}>
       {workshopMembers.map((wm) => {
         return (
           <i key={wm.id} className={cn("fa fa-user", wm.online ? "text-muted" : "text-primary")} />
--- src/pages/Workshop/library/MoveOn/index.js
+++ src/pages/Workshop/library/MoveOn/index.js
@@ -5,12 +5,84 @@ import OnlineMembers from './OnlineMembers';
 import "./MoveOn.scss";
 
 const MoveOn = () => {
-  return (
-    <div className="move-on-container">
-      <button className="btn btn-primary px-4 btn-block mb-2">Move On</button>
+  const [showConfirmation, setShowConfirmation] = React.useState(false);
+  const [readyUp, setReadyUp] = React.useState(false);
 
-      <OnlineMembers />
-    </div>
+  return (
+    <React.Fragment>
+      {readyUp ?
+        <div className="modal-backdrop fade show" />
+      : null}
+
+      <div className="move-on-container">
+        {readyUp ?
+          <div className="move-on-dialog border border-black shadow p-3 mb-2">
+            <div className="h5 mb-2">Waiting on the group</div>
+
+            <OnlineMembers
+              className="mb-2"
+            />
+
+            <div className="h5 mb-2">Forgot you had something else to fill out?</div>
+
+            <div>
+              <button
+                onClick={() => setReadyUp(false)}
+                className="btn btn-primary btn-square rounded px-4"
+              >
+                Go back
+              </button>
+            </div>
+          </div>
+        : null}
+
+        {showConfirmation ?
+          <div className="move-on-dialog border border-black shadow px-3 pb-3 mb-2">
+            <div className="text-right">
+              <button
+                onClick={() => setShowConfirmation(false)}
+                className="btn small btn-link text-danger btn-square"
+              >
+                Close
+              </button>
+            </div>
+
+            <div className="h4 mb-2">Are you ready to move on?</div>
+
+            <div className="mb-2">
+              If you are finished before the time is up, you can click this and notify the team you are ready to move on.
+            </div>
+
+            <div>
+              <button
+                onClick={() => {
+                  setReadyUp(true);
+                  setShowConfirmation(false);
+                }}
+                className="btn btn-primary btn-square rounded px-4"
+              >
+                I'm ready
+              </button>
+            </div>
+          </div>
+        : null}
+
+        {!showConfirmation && !readyUp ?
+          <button
+            onClick={() => setShowConfirmation(true)}
+            className="btn btn-primary px-4 btn-block mb-2"
+          >
+            Move On
+          </button>
+        : null}
+
+        {!readyUp ?
+          <OnlineMembers
+            className="justify-content-end"
+          />
+        : null}
+      </div>
+    </React.Fragment>
   );
 }
 
