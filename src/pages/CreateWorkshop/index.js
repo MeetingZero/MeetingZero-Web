59fd58e1c76db107e3449d600b28683d11e507fd
Arun Sood <arsood@gmail.com>
2020-11-30 19:31:16 -0800

    Start work on new layout for config page

--- src/pages/CreateWorkshop/index.js
+++ src/pages/CreateWorkshop/index.js
@@ -6,13 +6,14 @@ import cn from 'classnames';
 import DateTimePicker from 'react-datetime-picker';
 import moment from 'moment';
 
-import LogoSplitLayout from 'layouts/LogoSplit';
 import Button from 'library/Button';
 import CharacterCounter from 'library/CharacterCounter';
 import TagsInput from 'library/TagsInput';
 import TextArea from 'library/TextArea';
 
 import * as workshopActions from 'app/workshop/actions';
+
+import "./CreateWorkshop.scss";
 
 const CreateWorkshop = () => {
   const dispatch = useDispatch();
@@ -42,7 +43,6 @@ const CreateWorkshop = () => {
 
     return dispatch(workshopActions.createWorkshop(formData, emails, dateTimeSelectedUtc))
     .then(() => {
-      // history.push(`/workshop/${newWorkshop.workshop_token}/start`);
       history.push("/create-workshop-confirmation");
     });
   }
@@ -67,98 +67,112 @@ const CreateWorkshop = () => {
   }
 
   return (
-    <LogoSplitLayout>
-      <div className="p-2">
-        <div className="text-right mb-5">
-          <Button href="/dashboard" text="Cancel" />
-        </div>
+    <div className="container-fluid container-fixed">
+      <div className="row">
+        <div className="col-8">
+          <div className="p-2">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="row mb-5">
+                <div className="col-4"></div>
 
-        <div className="container">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-5 text-center">
-              Create Workshop
-            </div>
+                <div className="col-4">
+                  <h3 className="text-center font-weight-bold">
+                    Create Workshop
+                  </h3>
+                </div>
 
-            <TextArea
-              control={control}
-              register={register({ required: true, maxLength: 140 })}
-              name="purpose"
-              placeholder="State a broad topic..."
-              className={cn("mb-1", charCountExceeded ? 'bg-scary' : '')}
-              onUserInput={(userInput) => setWorkshopPurpose(userInput)}
-            />
-
-            <div className="row mb-10">
-              <div className="col-6">
-                {errors.purpose ?
-                  <div className="small text-danger">
-                    Please enter a workshop name of 140 characters or less
+                <div className="col-4">
+                  <div className="text-right">
+                    <Button href="/dashboard" text="Cancel" />
                   </div>
-                : null}
-              </div>
-
-              <div className="col-6">
-                <CharacterCounter
-                  className="text-right"
-                  maxChars={140}
-                  inputString={workshopPurpose}
-                  onExceed={handleExceed}
-                />
-              </div>
-            </div>
-
-            <TagsInput
-              className="form-control border-top-0 border-left-0 border-right-0 rounded-0 mb-1"
-              value={emails}
-              onChange={handleEmailChange}
-              onlyUnique={true}
-              placeholder="Invite Attendees"
-            />
-
-            <div className="row mb-2">
-              <div className="col-6">
-                  {showError ?
-                    <div className="text-danger">
-                      There must be between 3 and 9 people in this workshop
-                    </div>
-                  : null}
-
-                  {showWarning ?
-                    <div className="text-warning">
-                      MeetingZero works best with more than 5 people
-                    </div>
-                  : null}
-              </div>
-
-              <div className="col-6">
-                <div className="text-right mb-2">
-                  Separate emails with the tab or enter key
                 </div>
               </div>
-            </div>
 
-            <div className="mb-2">
-              Select Date and Time
-            </div>
-
-            <div className="mb-5">
-              <DateTimePicker 
-                value={dateTimeSelected}
-                onChange={setDateTimeSelected}
+              <TextArea
+                control={control}
+                register={register({ required: true, maxLength: 140 })}
+                name="purpose"
+                placeholder="State a broad topic..."
+                className={cn("mb-1", charCountExceeded ? 'bg-scary' : '')}
+                onUserInput={(userInput) => setWorkshopPurpose(userInput)}
               />
-            </div>
 
-            <div className="text-center mb-1">
-              <Button type="submit" className="btn btn-primary px-5" text="Create workshop" loading={isLoading} />
-            </div>
+              <div className="row mb-10">
+                <div className="col-6">
+                  {errors.purpose ?
+                    <div className="small text-danger">
+                      Please enter a workshop name of 140 characters or less
+                    </div>
+                  : null}
+                </div>
 
-            <div className="mx-auto text-center text-muted small" style={{maxWidth: 200}}>
-              Clicking create will send an invitation link and ID to members
-            </div>
-          </form>
+                <div className="col-6">
+                  <CharacterCounter
+                    className="text-right"
+                    maxChars={140}
+                    inputString={workshopPurpose}
+                    onExceed={handleExceed}
+                  />
+                </div>
+              </div>
+
+              <TagsInput
+                className="form-control border-top-0 border-left-0 border-right-0 rounded-0 mb-1"
+                value={emails}
+                onChange={handleEmailChange}
+                onlyUnique={true}
+                placeholder="Invite Attendees"
+              />
+
+              <div className="row mb-2">
+                <div className="col-6">
+                    {showError ?
+                      <div className="text-danger">
+                        There must be between 3 and 9 people in this workshop
+                      </div>
+                    : null}
+
+                    {showWarning ?
+                      <div className="text-warning">
+                        MeetingZero works best with more than 5 people
+                      </div>
+                    : null}
+                </div>
+
+                <div className="col-6">
+                  <div className="text-right mb-2">
+                    Separate emails with the tab or enter key
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-2">
+                Select Date and Time
+              </div>
+
+              <div className="mb-5">
+                <DateTimePicker 
+                  value={dateTimeSelected}
+                  onChange={setDateTimeSelected}
+                />
+              </div>
+
+              <div className="text-center mb-1">
+                <Button type="submit" className="btn btn-primary px-5" text="Create workshop" loading={isLoading} />
+              </div>
+
+              <div className="mx-auto text-center text-muted small" style={{maxWidth: 200}}>
+                Clicking create will send an invitation link and ID to members
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="create-workshop-helper-column col-4 vh-100">
+          Column Here
         </div>
       </div>
-    </LogoSplitLayout>
+    </div>
   );
 }
 
