5b3f1c718342ab05bc302ec475a14e16d5b05a06
Arun Sood <arsood@gmail.com>
2021-01-22 00:57:07 -0800

    Dynamic textareas with remove, add, and submit functionality

--- src/library/TextArea/index.js
+++ src/library/TextArea/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useWatch, useFieldArray } from 'react-hook-form';
 import cn from 'classnames';
 
+import "./TextArea.scss";
+
 const Textarea = ({
   control,
   register,
@@ -38,8 +40,12 @@ export const DynamicTextarea = ({
   className,
   placeholder,
   onUserInput,
-  startingNumInputs = 1
+  onSubmit,
+  startingNumInputs = 1,
+  removable = false
 }) => {
+  const [submitDisabled, setSubmitDisabled] = React.useState(true);
+
   const userInput = useWatch({
     control: formInstance.control,
     name: name,
@@ -54,6 +60,10 @@ export const DynamicTextarea = ({
   React.useEffect(() => {
     if (onUserInput) {
       onUserInput(userInput);
+    }
+
+    if (userInput && userInput[0].value.length) {
+      setSubmitDisabled(false);
     }
   }, [userInput, onUserInput]);
 
@@ -72,15 +82,46 @@ export const DynamicTextarea = ({
   return (
     <React.Fragment>
       {fields.map((field, index) => (
-        <textarea
-          key={field.id}
-          name={`${name}[${index}].value`}
-          ref={formInstance.register()}
-          defaultValue={field.value}
-          className={cn("form-control", className)}
-          placeholder={placeholder}
-        />
+        <div key={field.id} className={cn(removable ? "removable-textarea" : null)}>
+          {removable ?
+            <i
+              onClick={() => remove(index)}
+              className="fa fa-trash remove-toggle"
+            />
+          : null}
+
+          <textarea
+            key={field.id}
+            name={`${name}[${index}].value`}
+            ref={formInstance.register()}
+            defaultValue={field.value}
+            className={cn("form-control", className)}
+            placeholder={typeof placeholder === "string" ? placeholder : placeholder(index)}
+          />
+        </div>
       ))}
+
+      <div className="text-right">
+        <button
+          onClick={() => append({value: ""})}
+          type="button"
+          className="btn btn-secondary btn-rounded px-2 py-1"
+        >
+          Add More
+          <i className="fa fa-plus ml-1" />
+        </button>
+
+        {onSubmit ?
+          <button
+            onClick={onSubmit}
+            type="button"
+            className="btn btn-primary btn-rounded px-2 py-1 ml-2"
+            disabled={submitDisabled}
+          >
+            Submit
+          </button>
+        : null}
+      </div>
     </React.Fragment>
   );
 }
--- src/pages/CreateWorkshop/ExistingProblems.js
+++ src/pages/CreateWorkshop/ExistingProblems.js
@@ -2,28 +2,12 @@ import React from 'react';
 
 import { HUMAN_COUNTING } from 'constants/englishNumbers';
 
-import LimitedTextarea from 'library/TextArea/LimitedTextarea';
 import { DynamicTextarea } from 'library/TextArea/index';
 
 const ExistingProblems = ({
   formInstance,
   setPssConfigComplete
 }) => {
-  const [problems, setProblems] = React.useState([null, null, null]);
-  const [submitDisabled, setSubmitDisabled] = React.useState(true);
-
-  const handleChange = () => {
-    const existingProblems = formInstance
-    .getValues()
-    .existing_problems;
-
-    if (existingProblems[0] && existingProblems[0].length) {
-      return setSubmitDisabled(false);
-    }
-
-    console.log(existingProblems)
-  }
-
   return (
     <React.Fragment>
       <div className="mt-4 mb-2">
@@ -31,63 +15,17 @@ const ExistingProblems = ({
           Give us a little more information
         </div>
 
-        <div className="create-workshop-removable-input">
-          <i
-            className="fa fa-trash remove-toggle"
-          />
-
-          <DynamicTextarea
-            formInstance={formInstance}
-            name="existing_problems"
-            startingNumInputs={3}
-          />
-        </div>
-
-        {/* {problems.map((problem, index) => {
-          return (
-            <div key={index} className="mb-2">
-              <div className="create-workshop-removable-input">
-                <i
-                  onClick={() => setProblems(problems.filter((p, i) => i !== index))}
-                  className="fa fa-trash remove-toggle"
-                />
-
-                <LimitedTextarea
-                  onUserInput={handleChange}
-                  formInstance={formInstance}
-                  fieldName={`existing_problems[${index}]`}
-                  fieldRequired={index === 0}
-                  placeholder={`${HUMAN_COUNTING[index + 1]} problem`}
-                  errorMessage="Please enter a problem of 140 characters or less"
-                />
-              </div>
-            </div>
-          );
-        })} */}
-      </div>
-
-      <div className="text-right mb-4">
-        <button
-          onClick={() => setProblems([...problems, null])}
-          type="button"
-          className="btn btn-secondary btn-rounded px-2 py-1"
-          disabled={problems.length === 27}
-        >
-          Add More
-          <i className="fa fa-plus ml-1" />
-        </button>
-
-        <button
-          onClick={() => {
-            console.log(formInstance.getValues())
-            setPssConfigComplete(true)
+        <DynamicTextarea
+          formInstance={formInstance}
+          name="existing_problems"
+          startingNumInputs={3}
+          removable={true}
+          placeholder={(index) => {
+            return `${HUMAN_COUNTING[index + 1]} problem`;
           }}
-          type="button"
-          className="btn btn-primary px-2 py-1 ml-2"
-          // disabled={submitDisabled}
-        >
-          Submit
-        </button>
+          className="mb-3"
+          onSubmit={() => setPssConfigComplete(true)}
+        />
       </div>
     </React.Fragment>
   );
