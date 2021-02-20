440871275ee3c5841263306e53ca5abf4885e4eb
Michael Wu <wu.michael.m@gmail.com>
2021-02-20 18:51:18 -0500

    rephrase titles

--- src/pages/PastWorkshops/ResultItems.js
+++ src/pages/PastWorkshops/ResultItems.js
@@ -7,16 +7,16 @@ const ResultItems = ({workshopSummary}) => {
       <h5 className="font-weight-normal mb-2">All Results</h5>
       <div className="mb-8">
         {workshopSummary.what_is_working_responses ? 
-          <CollapsibleResult title="What's Working" items={workshopSummary.what_is_working_responses} /> 
+          <CollapsibleResult title="What's Working Submissions" items={workshopSummary.what_is_working_responses} /> 
         : null}
         {workshopSummary.problem_responses ?
-          <CollapsibleResult title="Original Problem" items={workshopSummary.problem_responses} />
+          <CollapsibleResult title="Original Problem Submissions" items={workshopSummary.problem_responses} />
         : null}
         {workshopSummary.reframe_problem_responses ?
-          <CollapsibleResult title="Reframed Problem" items={workshopSummary.reframe_problem_responses} />
+          <CollapsibleResult title="Reframed Problem Submissions" items={workshopSummary.reframe_problem_responses} />
         : null}
         {workshopSummary.solution_responses ?
-          <CollapsibleResult title="Solutions" items={workshopSummary.solution_responses} group={true} />
+          <CollapsibleResult title="Solution Submissions" items={workshopSummary.solution_responses} group={true} />
         : null}
       </div>
     </React.Fragment>
